f71d7db463423ee207d19a27a6ed9ef0aadec973
LiuWenYuan <15816141883@163.com>
2019-07-25 13:46:24 +0800

    Initial commit

---
+++ piouX.Core/Commands.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace piouX.Core
+{
+    public class Commands
+    {
+        public class MsgBox
+        {
+            public static void CmdMsgBox(string text, string caption, MsgBox.Type type)
+            {
+                MessageBox.Show(text, caption, type.but, type.icn, type.dfBut, type.opt);
+            }
+
+            public class Type
+            {
+                public MessageBoxButtons but;
+                public MessageBoxDefaultButton dfBut;
+                public MessageBoxIcon icn;
+                public MessageBoxOptions opt;
+                public Type(
+                    MessageBoxButtons buttons,
+                    MessageBoxDefaultButton defButton,
+                    MessageBoxIcon icon,
+                    MessageBoxOptions options
+                )
+                {
+                    but = buttons;
+                    dfBut = defButton;
+                    icn = icon;
+                    opt = options;
+                }
+            }
+        }
+    }
+}
---
+++ piouX.GUI/GUIMain.cs
@@ -0,0 +1,20 @@
+﻿using piouX.Core;
+using System;
+using System.Windows.Forms;
+
+namespace piouX.GUI
+{
+    public partial class GUIMain : Form
+    {
+        public GUIMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Commands.MsgBox.Type type = new Commands.MsgBox.Type(MessageBoxButtons.OK, MessageBoxDefaultButton.Button1, MessageBoxIcon.Information, 0);
+            Commands.MsgBox.CmdMsgBox("Hello world!", Text, type);
+        }
+    }
+}
